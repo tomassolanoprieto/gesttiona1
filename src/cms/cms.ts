bb493792cadbbc68a2aebb37a7199c6726c57fba
tomassolanoprieto <162082823+tomassolanoprieto@users.noreply.github.com>
2024-12-18 15:21:07 +0100

    Delete src directory

--- src/cms/cms.ts
+++
@@ -1,9 +0,0 @@
-import CMS from 'decap-cms-app';
-import { es } from 'decap-cms-locales';
-import { config } from './config';
-
-// Register Spanish locale
-CMS.registerLocale('es', es);
-
-// Initialize CMS with config
-CMS.init({ config });
--- src/types.ts
+++
@@ -1,7 +0,0 @@
-export interface ContactInfo {
-  email: string;
-  phone: string;
-  address: string;
-  officeHours: string;
-  businessHours: string;
-}
